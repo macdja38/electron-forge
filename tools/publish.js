b525409ca35b1c441f2a06b4c6a4b61a7f12bd68
Keeley Hammond <khammond@slack-corp.com>
2022-10-24 15:57:57 -0700

    chore: remove sync-readmes script

--- tools/publish.js
+++ tools/publish.js
@@ -31,12 +31,6 @@ const prepare = new Listr([
         cwd: BASE_DIR,
       }),
   },
-  {
-    title: "Fetching README's",
-    // Why: somehow, referencing a TS file in a JS file works?
-    // eslint-disable-next-line node/no-missing-require
-    task: require('./sync-readmes'),
-  },
 ]);
 
 const publisher = new Listr([
